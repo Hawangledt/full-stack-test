e599f83779bbb7931b6ca09c9898400f41cfbee4
Rawan.H <rawanhawangledt@gmail.com>
2023-07-21 15:00:30 -0300

    initial project setup and login system
    Creating the project's folder structure, writing pattern and login system as a basis for new features.

---
+++ src/models/user.js
@@ -0,0 +1,36 @@
+const { connection } = require('../database/connection')
+const { STRING } = require('sequelize')
+const bcrypt = require('bcrypt')
+
+
+const User = connection.define("user", {
+    name: STRING,
+    email: {
+        type: STRING,
+        validate: {
+            isEmail: { msg: "Email must be between 5 to 100 characters" },
+            len: { args: [1, 100] }
+        },
+        unique: { msg: "E-mail already registered." }
+    },
+    password: {
+        type: STRING,
+        validate: {
+            len: { args: [5, 100], msg: "Password must be between 5 to 100 characters" },
+            is: {
+                args: "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{5,})$",
+                msg: "Password entered too weak. It must have a number, an uppercase letter and a lowercase letter."
+            }
+        },
+    }
+}, {
+    underscored: true, paranoid: true, hooks: {
+        beforeCreate: (user) => {
+            user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(8))
+        }
+    }
+})
+
+
+
+module.exports = { User }
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+const { routesFromUser } = require('./user.routes')
+
+
+const routes = new Router()
+
+routes.use('/api', [
+  routesFromUser(),
+])
+
+module.exports = routes
